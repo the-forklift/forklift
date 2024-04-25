7568ed57a20f368ad47efd6538d4b295133c18f7
ardi <ardis@ardi.dev>
2024-04-25 04:11:25 +0200

    Rough mvp

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,126 @@
-use db_dump;
+use std::{
+    collections::{hash_map::Entry, HashMap, HashSet},
+    env,
+    fmt::Display,
+    time::Instant,
+};
 
-struct CrateInfo {
-    cratename: Option<String>,
-    tree_level: u8,
-}
-fn maketree(crate_name: Option<String>, tree_level: u8, stack: &mut Vec<CrateInfo>) {
-    /*
-       ____________________________________________________________________________
-    * |                                                                           |
-    * | chances are, we're going to implement tree_level by having the input crate|
-    * | have a tree_level of 0, and then for every other crate that appears, the  |
-    * | crates that it is dependent on are equiv. of currentcrte.tree_level + 1,  |
-    * | and this will be used to visually represent a tree                        |
-    * |___________________________________________________________________________|
+use db_dump::{self, crates::CrateId, versions::VersionId};
+use semver::{Version, VersionReq};
 
-    */ 
-    
-    let _ = db_dump::Loader::new().dependencies(|row| {
-        stack.push(CrateInfo{ 
-            cratename: row.explicit_name,
-            tree_level: tree_level + 1,
-        }); 
-    }).load("./db-dump.tar.gz");
-    
-    while let Some(cratestack) = stack.pop() {
-        maketree(cratestack.cratename, cratestack.tree_level, stack);
-    }
+type Crates = HashMap<CrateId, String>;
+type Versions = HashMap<VersionId, Vec<(CrateId, Version)>>;
+type Dependencies = HashMap<CrateId, Vec<Dependency>>;
+type Seen = HashSet<CrateId>;
 
+struct Dependency {
+    /// Id of the dependency
+    pub version_id: VersionId,
+    pub req: VersionReq,
 }
 
-fn main() -> db_dump::Result<()> {
+struct Tree {
+    id: CrateId,
+    version: Version,
+    rev_deps: Vec<Tree>,
+}
 
+impl Tree {
+    fn recurse(&mut self, dependencies: &Dependencies, vers: &Versions, seen: &mut Seen) {
+        let Some(reverse_deps) = dependencies.get(&self.id) else {
+            return;
+        };
 
-    let mut dependancy_tree: Vec<CrateInfo> = Vec::new(); //u32: crate ID, u8: level in the tree 
-    //____________________________________________________________________________________
-    //need to make something which takes user input and pushes it into the dependancy tree|
-    //____________________________________________________________________________________
+        let trees = reverse_deps
+            .iter()
+            .filter(|dep| dep.req.matches(&self.version)) // Only get our version
+            .filter_map(|dep| vers.get(&dep.version_id))
+            .flatten()
+            .map(|x| Tree {
+                id: x.0,
+                version: x.1.clone(),
+                rev_deps: vec![],
+            });
 
-    if let Some(last_crate) = dependancy_tree.last() {
-        let stack_cratename = last_crate.cratename.clone();
-        let stack_treelevel = last_crate.tree_level;
-        maketree(stack_cratename, stack_treelevel, &mut dependancy_tree);
+        for mut tree in trees {
+            if seen.insert(tree.id) {
+                // First time we see it
+                tree.recurse(dependencies, vers, seen);
+            }
+            self.rev_deps.push(tree);
+        }
     }
-    Ok(())
+
+    fn get_crates(&self, crates: &Crates, s: &mut String) {
+        let crate_name = crates.get(&self.id).unwrap();
+        s.push_str(&format!("{crate_name}: {}\n", self.version));
+        for i in &self.rev_deps {
+            i.get_crates(crates, s);
+        }
+    }
+}
+
+fn main() {
+    // Just for testing
+    let mut args = env::args().skip(1);
+    let crate_name = args.next().expect("Usage: forklift [crate] [version]");
+    let version: Version = args
+        .next() //.unwrap_or("*".into()) TODO: Use VersionReq instead
+        .expect("Usage: forklift [crate] [version]")
+        .parse()
+        .unwrap();
+
+    // PERF: Use with_capacity
+    let mut dependencies: Dependencies = HashMap::new();
+    let mut versions: Versions = HashMap::new();
+    let mut crates: Crates = HashMap::new();
+    let mut seen: Seen = HashSet::new();
+
+    let mut tree = None;
+    db_dump::Loader::new()
+        .crates(|row| {
+            if row.name == crate_name {
+                seen.insert(row.id);
+                tree = Some(Tree {
+                    id: row.id,
+                    version: version.clone(),
+                    rev_deps: vec![],
+                })
+            }
+            crates.insert(row.id, row.name);
+        })
+        .versions(|row| {
+            let val = (row.crate_id, row.num);
+            match versions.entry(row.id) {
+                Entry::Occupied(mut vec) => vec.get_mut().push(val),
+                Entry::Vacant(a) => {
+                    a.insert(vec![val]);
+                }
+            };
+        })
+        .dependencies(|row| {
+            let val = Dependency {
+                req: row.req,
+                version_id: row.version_id,
+            };
+
+            match dependencies.entry(row.crate_id) {
+                Entry::Occupied(mut vec) => vec.get_mut().push(val),
+                Entry::Vacant(a) => {
+                    a.insert(vec![val]);
+                }
+            };
+        })
+        .load("./db-dump.tar.gz")
+        .expect("Missing 'db-dump.tar.gz'. This can be downloaded from https://static.crates.io/db-dump.tar.gz");
+
+    let mut tree = tree.expect("crate not found!");
+
+    tree.recurse(&dependencies, &versions, &mut seen);
+
+    let mut out = String::new();
+    tree.get_crates(&crates, &mut out);
+
+    println!("{out}");
+    println!("found {} reverse dependencies!", seen.len());
 }
